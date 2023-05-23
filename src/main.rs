ab662e0e115172079d6225405deac0e45efcb9e4
HasanTusher <hasan.tusher787@gmail.com>
2023-05-24 03:11:18 +0600

    everything done

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,20 @@ extern crate greprs;
 
 use greprs::Config;
 use std::env;
+use std::io::prelude::*;
 use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    let mut stderr = std::io::stderr();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        writeln!(&mut stderr, "Problem parsing arguments: {}", err)
+            .expect("Could not write to stderr");
         process::exit(1);
     });
     if let Err(e) = greprs::run(config) {
-        println!("Application error: {}", e);
+        writeln!(&mut stderr, "Application error: {}", e).expect("Could not write to stderr");
         process::exit(1);
     }
 }

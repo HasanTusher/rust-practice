67ee682f6cde66f27261a20972dc0145388e53c3
HasanTusher <hasan.tusher787@gmail.com>
2023-05-24 01:06:36 +0600

    lib rs segregated

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+extern crate greprs;
+
+use greprs::Config;
 use std::env;
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
 
 fn main() {
@@ -12,36 +12,8 @@ fn main() {
     });
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
-    if let Err(e) = run(config) {
+    if let Err(e) = greprs::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut f = File::open(config.filename)?;
-
-    let mut contents = String::new();
-
-    f.read_to_string(&mut contents)?;
-
-    println!("With text:\n {}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = String::from(&args[1]);
-        let filename = String::from(&args[2]);
-        Ok(Config { query, filename })
     }
 }

5fc7424e5d740eef8f2fa6a5cab699d14b41f545
HasanTusher <hasan.tusher787@gmail.com>
2023-05-24 00:29:54 +0600

    extracted run

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
+    run(config);
+}
 
+fn run(config: Config) {
     let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
@@ -24,12 +31,12 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
         let query = String::from(&args[1]);
         let filename = String::from(&args[2]);
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
